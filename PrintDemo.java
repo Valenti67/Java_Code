57db8257ac16166af87fdf4dd94e6808d55be86c
Valenti67 <55367761+Valenti67@users.noreply.github.com>
2020-12-22 22:42:16 +0800

    基础语法

---
+++ PrintDemo.java
@@ -0,0 +1,11 @@
+public class PrintDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.print("Hello");
+        System.out.printf("%s %d \n", "hello", 10);
+
+        
+        System.out.format("%s %d \n", "hello", 10);
+        System.out.println("hello" + " " + 10 + " " + "\n");
+    }
+}
---
+++ StringDemo.java
@@ -0,0 +1,32 @@
+import org.omg.CORBA.INTERNAL;
+
+public class StringDemo {
+    public static void main(String[] args) {
+//        String s = "你好中国";      // 引用类型
+////        String t = s;
+////        System.out.println(t);
+////        System.out.println(s);
+//
+//        //s = s + 10;     // + 字符串拼接(concat)
+//        // 就等于下面的几句语句
+//        String s1 = String.valueOf(10);  // 等效于 String s1 = "10";
+//        s = s.concat(s1);
+//
+//        System.out.println(s);
+//
+//        String t = 10 + "hello";
+//        System.out.println(t);
+//
+//        String r = "hello" + "world";
+
+//        String s = "10";
+//        int i = Integer.parseInt(s);    // i = Integer.valueOf(s);
+//        System.out.println(i + 1);
+//        long l = Long.valueOf(s);
+//        System.out.println(l + 2);
+
+//        String s = "Hello";
+//        int i = Integer.parseInt(s);
+//        System.out.println(i);    // 会出现运行异常
+    }
+}
