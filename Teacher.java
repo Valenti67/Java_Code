48c0e6dbcbd2a6cbc663408d881c9f37fdb4fac9
Valenti67 <55367761+Valenti67@users.noreply.github.com>
2021-01-23 17:19:54 +0800

    类和对象1

--- Teacher.java
+++ Teacher.java
@@ -1,9 +1,26 @@
 public class Teacher {
-    // 目前，所有属性的定义全部带上 public
-    // 属性的数据类型不限，可以是基本类型 OR 引用类型
-    // 书写位置在类的内部 && 方法的外部
-    // 定义规则：修饰符 变量类型 变量名 —— 这类变量（属性）
-    // 属性也遵循默认值规则 —— 默认值都是 0 的变形
     public String name;
     public int age;
+
+    // 没有写构造方法
+    // 如果类里，一个构造方法都没有写
+    // 则 Java 隐含着补一个构造方法，无参 + 什么都不做
+
+    // 什么都不写，等同于写了这个
+//    public Teacher() {
+//    }
+
+    // 但是，一旦类里有构造方法了，无论长什么样，Java 不再补无参的构造方法了
+    public Teacher(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    // 没有无参构造方法了
+
+    public static void main(String[] args) {
+        //Teacher a = new Teacher();      // 没有写构造方法，但我也可以构造出对象
+        Teacher a = new Teacher("Hello", 18);
+        System.out.println(a.name);     // 访问属性
+        System.out.println(a.age);
+    }
 }
